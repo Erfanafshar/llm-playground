317afcc8bb5ddb0c9df0c35c60991e2eb05f74b9
Erfan Afshar <afshar.erfan@gmail.com>
2025-11-07 23:01:01 +0000

    1st verion of full assistant

---
+++ main_assistant.py
@@ -0,0 +1,43 @@
+from openai import OpenAI
+import os, json
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+# ---- Tool functions -----
+def search_notes(query, top_k=3):
+    pass
+
+def get_web_data(topic):
+    pass
+
+# --- Tools regsigery -------
+tools = [
+    
+]
+
+def run():
+    print("AI assistant ready, (type 'exit' to quit)\n")
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant that can use tools to find or fetch facts."}
+    ]
+
+    while True:
+        user = input("You: ")
+        if user.lower() == "exit":
+            print("Assistant: Bye!")
+            break
+        
+        messages.append(
+            {"role": "user", "content": user}
+        )
+
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages = messages,
+            temperature = 0.5
+        )
+
+        print(f"Assistant: {response.choices[0].message.content}")
+
+if __name__ == "__main__":
+    run()
